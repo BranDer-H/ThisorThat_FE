dc7aefb5f9c33c5e006a20f34235f9d597ade2b9
Derek-94 <ghdtjrrl94@gmail.com>
2021-07-23 12:08:56 +0900

    Updates

---
+++ precache-manifest.37a1637f16b2b3c0a8071413b4b88b55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d136f5be692480a7aca82fd0ab81296a",
+    "url": "/thisorthat_fe/index.html"
+  },
+  {
+    "revision": "31348ccd87bb24e67ea5",
+    "url": "/thisorthat_fe/static/css/2.ecfb3f68.chunk.css"
+  },
+  {
+    "revision": "465a6b7e4aee4ad4cb15",
+    "url": "/thisorthat_fe/static/css/main.424d9979.chunk.css"
+  },
+  {
+    "revision": "31348ccd87bb24e67ea5",
+    "url": "/thisorthat_fe/static/js/2.6b3b95a7.chunk.js"
+  },
+  {
+    "revision": "38347b19e5a3c3fe4f8e725a18e86843",
+    "url": "/thisorthat_fe/static/js/2.6b3b95a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04301d93057c2c6eecf",
+    "url": "/thisorthat_fe/static/js/3.09cd0b44.chunk.js"
+  },
+  {
+    "revision": "465a6b7e4aee4ad4cb15",
+    "url": "/thisorthat_fe/static/js/main.d7aa74ba.chunk.js"
+  },
+  {
+    "revision": "fb1a4ac7cfe48fe33fcd",
+    "url": "/thisorthat_fe/static/js/runtime-main.a75db124.js"
+  },
+  {
+    "revision": "b6c8137dd2e215ed1aba2ebfc45c93c4",
+    "url": "/thisorthat_fe/static/media/cookierun.b6c8137d.TTF"
+  }
+]);
