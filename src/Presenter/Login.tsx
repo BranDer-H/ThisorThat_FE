d547c7df2b8f4031ef6579183273357b4913e2f2
Derek-94 <ghdtjrrl94@gmail.com>
2021-07-24 15:38:54 +0900

    bug: fixed the bugs.
    - Add login button in login screen.
    - Activate send button.
    - Change user name color.

--- src/Presenter/Login.tsx
+++ src/Presenter/Login.tsx
@@ -15,15 +15,19 @@ const LoginForm = styled.form`
 `;
 
 const LoginTitle = styled.div`
-  font-size: 1.5rem;
+  font-family: 'cookie';
+  font-size: 1.4rem;
+  font-weight: 600;
   margin: 0.5rem;
-  margin-bottom: 2.5rem;
-  color: silver;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+  color: white;
 `;
 
 const LoginInput = styled(Input)`
   font-size: 1.5rem;
   font-weight: 400;
+  font-family: 'cookie';
   text-align: center;
   width: 100%;
   color: black;
@@ -31,20 +35,35 @@ const LoginInput = styled(Input)`
 `;
 
 const LoginSubmit = styled.input`
-  display: none;
+  font-size: 1.2rem;
+  font-family: 'cookie';
+  font-weight: 600;
+  color: black;
+  padding: 0.5rem;
+  background-color: silver;
+  border: 0px;
+  border-radius: 20%;
+  cursor: pointer;
+  :hover  {
+    background-color: black;
+    color: silver;
+    transition: 600ms ease;
+  }
 `;
 
 const Login = ({ userNickname, setUserNickname, onRegisterNickName }: any) => (
   <LoginForm onSubmit={onRegisterNickName}>
-    <LoginTitle>닉네임을 입력하세요.</LoginTitle>
+    <LoginTitle>내 이름은</LoginTitle>
     <LoginInput
       value={userNickname}
       autoFocus
       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
         setUserNickname(e.target.value)
       }
+      required
     />
-    <LoginSubmit type="submit" value="" />
+    <LoginTitle>마시멜로우로 할래요!</LoginTitle>
+    <LoginSubmit type="submit" value="결정" />
   </LoginForm>
 );
 
